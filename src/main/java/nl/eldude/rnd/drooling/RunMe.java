2511fbfe37756547103c8a5dcaaa4faa711f4588
gabe2001 <gabe2001@users.noreply.github.com>
2019-11-26 22:07:48 +0100

    setting up still

--- src/main/java/nl/eldude/rnd/drooling/RunMe.java
+++ src/main/java/nl/eldude/rnd/drooling/RunMe.java
@@ -2,6 +2,7 @@ package nl.eldude.rnd.drooling;
 
 import nl.eldude.rnd.drooling.model.Order;
 import org.drools.compiler.compiler.DroolsParserException;
+import org.drools.compiler.rule.builder.util.PackageBuilderUtil;
 import org.drools.core.WorkingMemory;
 
 import java.io.IOException;
@@ -10,9 +11,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import org.drools.compiler.compiler.DroolsParserException;
-import org.drools.compiler.compiler.PackageBuilder;
-import org.drools.core.RuleBase;
-import org.drools.core.RuleBaseFactory;
 import org.drools.core.WorkingMemory;
 
 public class RunMe {
@@ -24,14 +22,14 @@ public class RunMe {
 
     public void executeDrools() throws DroolsParserException, IOException {
 
-        PackageBuilder packageBuilder = new PackageBuilder();
+        PackageBuilderUtil packageBuilder = new PackageBuilderUtil();
 
         String ruleFile = "/nl/eldude/rnd/drooling/rules/applyDiscounts.drl";
         InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
 
         Reader reader = new InputStreamReader(resourceAsStream);
         packageBuilder.addPackageFromDrl(reader);
-        org.drools.core.rule.Package rulesPackage = packageBuilder.getPackage();
+        Package rulesPackage = packageBuilder.getPackage();
         RuleBase ruleBase = RuleBaseFactory.newRuleBase();
         ruleBase.addPackage(rulesPackage);
 
